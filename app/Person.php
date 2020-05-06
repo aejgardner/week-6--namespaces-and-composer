69014b4d8ce3ab4c2bad3f614a458e2e9f518963
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-06 17:17:07 +0100

    Challenge 02 completed

---
+++ app/Person.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class Person
+{
+    private $firstName;
+    private $lastName;
+
+    public function __construct(string $firstName, string $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function sayHelloTo(Person $person) : string
+    {
+        return "Hello " . $person->getFullName();
+    }
+
+    public function getFullName() : string
+    {
+        return $this->firstName . " " . $this->lastName;
+    }
+}
