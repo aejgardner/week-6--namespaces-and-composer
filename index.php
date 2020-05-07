db4dd671b6e2dd9ee6f426aaec0610e57c077fa3
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-07 10:16:48 +0100

    Challenge 03 (Potato) completed

--- index.php
+++ index.php
@@ -28,3 +28,24 @@ $person2 = new Person("Wes", "Anderson");
 
 dump($person1->sayHelloTo($person2)); // string(9) "Hello Wes Anderson"
 dump($person2->sayHelloTo($person1)); // string(11) "Hello Lynne Ramsay"
+
+
+// Create a class Potato in the App\Stuff\Things namespace. It should have a water() and hasGrown() method. hasGrown() should return false until the Potato has been watered five or more times.
+
+echo "\nQuestion 3:\n";
+
+use App\Stuff\Things\Potato;
+
+$potato = new Potato();
+$potato->water()->water();
+
+dump($potato->hasGrown()); // false
+
+$potato->water()->water();
+dump($potato->hasGrown()); // false
+
+$potato->water();
+dump($potato->hasGrown()); // true
+
+$potato->water()->water();
+dump($potato->hasGrown()); // true
